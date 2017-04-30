0f8dac83374890984c6eee859d4bfdcc6f9496a8
Eyeball Edward <edwardyeung39@gmail.com>
2017-04-30 23:16:25 +0800

    Restructure and added in query system

--- models/ActiveBusServicesModel.js
+++ models/ActiveBusServicesModel.js
@@ -18,29 +18,55 @@ class ActiveBusServicesModel {
 		this.cacheUpdater();
 	}
 
+	isServiceActive(serviceNo, direction, callback) {
+        if (!this.cache.currentlyActiveServices) {callback(null);return}
+		for (let service of this.cache.currentlyActiveServices) {
+			if (service.serviceNo === serviceNo && service.direction.toString() === direction) {
+				callback(true, this.cache.lastRefreshTime);
+				return;
+			}
+		}
+		callback(false);
+	}
+
 	cacheUpdater() {
 		this.busServiceAPI.getBusServicesOperatingNow(services => {
-			var uniqueServices = {};
-			for (let service of services) 
-				if (!uniqueServices[service.serviceNo])
-					uniqueServices[service.serviceNo] = {dirs: [service.direction], svc: service.serviceNo};
-				else
-					uniqueServices[service.serviceNo].dirs.push(service.direction);
 			var list = {
-				currentlyActiveServices: Object.keys(uniqueServices).map(service => {
-					service = uniqueServices[service];
+				currentlyActiveServices: services.map(service => {
 					return {
-						serviceNo: `${service.svc}D{${service.dirs.reduce((a,b)=>a+','+b, '').substring(1)}}`,
+						serviceNo: service.serviceNo,
+						direction: service.direction,
 					};
-				}).filter(Boolean),
+				}),
 				lastRefreshTime: new Date()
 			};
 			this.cache = list;
-		});
+		}, 2);
 	}
 
 	getActiveBusServices(callback) {
-		callback(this.cache);
+
+		if (!this.cache.currentlyActiveServices) {callback({});return}
+		var services = this.cache.currentlyActiveServices;
+
+		var uniqueServices = {};
+		for (let service of services)
+			if (!uniqueServices[service.serviceNo])
+				uniqueServices[service.serviceNo] = {dirs: [service.direction], svc: service.serviceNo};
+			else
+				uniqueServices[service.serviceNo].dirs.push(service.direction);
+		var list = {
+			currentlyActiveServices: Object.keys(uniqueServices).map(service => {
+				service = uniqueServices[service];
+				return {
+					serviceNo: `${service.svc}D{${service.dirs.reduce((a,b)=>a+','+b, '').substring(1)}}`,
+				};
+			}).filter(Boolean),
+			lastRefreshTime: services.lastRefreshTime
+		};
+
+
+		callback(list);
 	}
 
 }
