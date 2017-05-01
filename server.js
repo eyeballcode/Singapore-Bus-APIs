07669ca19b015a3290d962dd1aa8f3daa45cd738
Eyeball Edward <edwardyeung39@gmail.com>
2017-05-01 21:00:22 +0800

    Use MongoDB

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 const express = require('express'),
-        bodyParser = require('body-parser'),
-	fs = require('fs'),
-	path = require('path');
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    path = require('path'),
+    {MongoClient, Server} = require('mongodb');
 
 const app = express(),
 	  config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'))),
       port = config.port || 8000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-	res.jsonError = (error) => {
-		res.status(400).json(error);
-	};
-	next();
-});
+var mongoClient = new MongoClient();
+mongoClient.connect('mongodb://localhost:27017/singapore-bus-apis', (err, database) => {
+    if (err) throw err;
 
-fs.readdir(path.join(__dirname, 'controllers'), (err, files) => {
-	files.filter(name => name !== 'Controller.js' && name.endsWith('.js')).forEach(name => {
-		const Controller = require(path.join(__dirname, 'controllers', name));
-		console.log(`Using router ${name.replace(/\.js$/, '')}.`);
-		app.use(Controller.mountPoint, new Controller().router);
-	});
-});
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use((req, res, next) => {
+	    res.jsonError = (error) => {
+		    res.status(400).json(error);
+	    };
+	    req.database = database;
+	    next();
+    });
 
-app.listen(port);
+    fs.readdir(path.join(__dirname, 'controllers'), (err, files) => {
+	    files.filter(name => name !== 'Controller.js' && name.endsWith('.js')).forEach(name => {
+		    const Controller = require(path.join(__dirname, 'controllers', name));
+		    console.log(`Using router ${name.replace(/\.js$/, '')}.`);
+		    app.use(Controller.mountPoint, new Controller().router);
+	    });
+    });
+
+    app.listen(port);
+});
